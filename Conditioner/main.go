d822fb6b6b1da6f55c243a449abbfab347e80d8e
sadceri <70819311+sadceri-dev@users.noreply.github.com>
2024-02-11 17:41:08 +0300

    Update main.go

--- Conditioner/main.go
+++ Conditioner/main.go
@@ -20,11 +20,12 @@ func tControl(troom, tcond int, mode string) int {
 	} else if mode == "fan" {
 		return troom
 	}
-
-	// Default return statement
 	return 0
 }
-
 func main() {
-	fmt.Println(tControl(30, 20, "fan")) // 20
+	var tr, tco int
+	var mod string
+	fmt.Scanln(&tr, &tco)
+	fmt.Scanln(&mod)
+	fmt.Println(tControl(tr, tco, mod))
 }
